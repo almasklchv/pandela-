c49afbd62557b8cbb7f7992b5ff6968b21a6e396
Almas Kilichov <itsmywaytoamerica@gmail.com>
2023-08-28 11:47:40 +0600

    delete all files

--- mainmenu/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainmenuConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainmenu'
-
-    def ready(self):
-        import mainmenu.signals
