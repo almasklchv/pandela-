fdb0b242f7400fabff7bc6d21c276e570398b317
Indert <valeriibrv@gmail.com>
2023-10-11 17:12:30 +0300

    find idshniki

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,18 +6,18 @@ urlpatterns = [
     path("create/", CreateBlogAPI.as_view(), name="blog_create"),
     path("manage/<str:pk>/", ManageBlogAPI.as_view(), name="blog_manage"),
     path(
-        "like/<str:blog_pk>/<str:writer_pk>/", LikeBlogAPI.as_view(), name="blog_like"
+        "like/<str:pk>/", LikeBlogAPI.as_view(), name="blog_like"
     ),
     path(
-        "save/<str:blog_pk>/<str:writer_pk>/", SaveBlogAPI.as_view(), name="blog_save"
+        "save/<str:pk>/", SaveBlogAPI.as_view(), name="blog_save"
     ),
     path('', BlogListView.as_view()),
     # path('category/<category_id>', BlogListCategoryView.as_view()),
-    path('video/<str:blog_pk>/', PostDetailView.as_view()),
+    path('video/<str:pk>/', PostDetailView.as_view()),
     path('search/<str:search_term>', BlogAdvancedSearchTermView.as_view()),
-    path("<str:blog_pk>//comment/", ListCommentAPIView.as_view(), name="list_comment"),
+    path("<str:blog_pk>/comment/", ListCommentAPIView.as_view(), name="list_comment"),
     path(
-        "<str:blog_pk>/<str:writer_pk>/comment/create/",
+        "<str:pk>/comment/create/",
         CreateCommentAPIView.as_view(),
         name="create_comment",
     ),
--- profiles/urls.py
+++ profiles/urls.py
@@ -3,19 +3,19 @@ from .views import *
 
 urlpatterns = [
     path("usernamesandemails/", UsernameAndEmails.as_view(), name="livecheck"),
-    path("setup/<int:pk>/", SetupProfileAPI.as_view(), name="acc_setup"),
+    path("setup/<str:pk>/", SetupProfileAPI.as_view(), name="acc_setup"),
     # path("manage/<int:pk>/", ManageProfileAPI.as_view(), name="acc_manage"),
-    path("delete/<int:pk>/", DeleteProfileAPI.as_view(), name="acc_delete"),
+    path("delete/<str:pk>/", DeleteProfileAPI.as_view(), name="acc_delete"),
     path("search/", SearchProfileAPI.as_view(), name="acc_search"),
     path(
-        "follow/<int:user_pk>/<int:profile_pk>/",
+        "follow/<str:pk>/",
         FollowProfileAPI.as_view(),
         name="acc_follow",
     ),
     path('edit-profile/', EditProfileView.as_view(), name='edit_profile'),
     # path для отдельного профиля с пафом видео/плейлистов/о канале
-    path('<int:id>/', UserProfileView.as_view(), name='profile'),
-    path('info/<int:ззл>/', UserInfoView.as_view(), name='profile'),
+    path('<str:pk>/', UserProfileView.as_view(), name='profile'),
+    path('info/<str:pk>/', UserInfoView.as_view(), name='profile'),
     path('account/', UserAccountView.as_view(), name='account'),
 path('account/archive', AccountArchiveView.as_view(), name='archive'),
     path('saves/', AccountSavesView.as_view(), name='saves'),
